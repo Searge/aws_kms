98df3641a28f4983b9346e565aee28bdbdfa616f
Searge <searge@duck.com>
2025-04-16 12:27:26 +0300

    :truck: Move envs to the kms dir

--- env/dev/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_region            = "eu-central-1"
-aws_access_key_id     = ""
-aws_secret_access_key = ""
--- kms/versions.tf
+++ kms/versions.tf
@@ -1,11 +1,11 @@
 terraform {
+  # Required Terraform version
+  required_version = "~> 1.11"
   required_providers {
     # AWS Provider
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.94"
     }
-    # Required Terraform version
-    required_version = ">= 1.10.0"
   }
 }
