e28a0b757ca10f280899f17e9d86c88ed1b98778
Searge <searge@duck.com>
2025-04-19 09:40:07 +0300

    :truck: Move old modules to archive

---
+++ .archive/modules/organization-policies/variables.tf
@@ -0,0 +1,22 @@
+variable "ou_map" {
+}
+
+variable "policies_directory" {
+  type    = string
+  default = null
+}
+
+variable "policy_type" {
+  type    = string
+  default = "SERVICE_CONTROL_POLICY"
+  validation {
+    condition = contains([
+      "AISERVICES_OPT_OUT_POLICY",
+      "BACKUP_POLICY",
+      "RESOURCE_CONTROL_POLICY",
+      "SERVICE_CONTROL_POLICY",
+      "TAG_POLICY"
+    ], var.policy_type)
+    error_message = "unsupported policy type"
+  }
+}
