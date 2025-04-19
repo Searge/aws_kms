551f10bee205075802c1eed0639bba6648b21e62
Searge <searge@duck.com>
2025-04-19 14:06:13 +0300

    refactor: Move variables

--- kms/variables.tf
+++ kms/variables.tf
@@ -4,11 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "env" {
-  type        = string
-  description = "Environment name"
-}
-
 variable "aws_access_key_id" {
   type        = string
   description = "AWS access key ID"
@@ -17,4 +12,21 @@ variable "aws_access_key_id" {
 variable "aws_secret_access_key" {
   type        = string
   description = "AWS secret access key"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default = ""
+}
+
+variable "tags" {
+  description = "The map of tags"
+  type        = map(string)
+  default     = {}
 }
