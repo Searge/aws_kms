cdfbab03efd9b64b772b08b43bd729ddddb02250
Searge <searge@duck.com>
2025-04-21 13:39:05 +0300

    chore: Add outputs for variables

---
+++ modules/org_policies/outputs.tf
@@ -0,0 +1,10 @@
+output "policies_directory" {
+  description = "Get the policies directory"
+  value       = local.policies_directory
+}
+
+# Output policy IDs
+output "policy_ids" {
+  description = "Get the policy IDs"
+  value       = local.policy_ids
+}
