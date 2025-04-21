993d3af109cf30289dcf614a768f64684d6a45a4
Searge <searge@duck.com>
2025-04-21 13:38:46 +0300

    :truck: Move old org to org_creation

--- org_creation/outputs.tf
+++ org_creation/outputs.tf
@@ -1,7 +1,7 @@
 
 output "organization_ids" {
   description = "Get the organization IDs"
-  value       = aws_organizations_organization.org.id
+  value       = data.aws_organizations_organization.org.id
 }
 
 output "org_accounts" {
