86d1669e8cc7a168005e94698545eeb743485a05
Searge <searge@duck.com>
2025-05-03 17:19:47 +0300

    fix: Bringing the tags to the same type

--- modules/kms_key/variables.tf
+++ modules/kms_key/variables.tf
@@ -1,16 +1,6 @@
 ###############################################################################
-# Project
+# Environment
 ###############################################################################
-# variable "project" {
-#   description = "Project name for deployment"
-#   type        = string
-
-#   validation {
-#     condition     = length(var.project) >= 3 && var.project != null && var.project != ""
-#     error_message = "Error: Incorrect project name."
-#   }
-# }
-
 variable "environment_name" {
   description = "Environment name for deployment"
   type        = string
@@ -37,51 +27,23 @@ variable "tags" {
 
   validation {
     condition = anytrue(
-      [for obj in keys(var.tags) : contains(["BU"], obj)]
+      [for obj in keys(var.tags) : contains(["owner"], obj)]
     )
-    error_message = "Tags map should include BU(Business Unit) key string."
+    error_message = "Tags map should include owner key string."
   }
 
   validation {
     condition = anytrue(
-      [for obj in keys(var.tags) : contains(["BusinessOwner"], obj)]
+      [for obj in keys(var.tags) : contains(["environment"], obj)]
     )
-    error_message = "Tags map should include BusinessOwner key string."
+    error_message = "Tags map should include environment key string."
   }
 
   validation {
     condition = anytrue(
-      [for obj in keys(var.tags) : contains(["TechnicalOwner"], obj)]
+      [for obj in keys(var.tags) : contains(["data-classification"], obj)]
     )
-    error_message = "Tags map should include TechnicalOwner key string."
-  }
-
-  validation {
-    condition = anytrue(
-      [for obj in keys(var.tags) : contains(["ProjectManager"], obj)]
-    )
-    error_message = "Tags map should include ProjectManager key string."
-  }
-
-  validation {
-    condition = anytrue(
-      [for obj in keys(var.tags) : contains(["Project"], obj)]
-    )
-    error_message = "Tags map should include Project key string."
-  }
-
-  validation {
-    condition = anytrue(
-      [for obj in keys(var.tags) : contains(["Owner"], obj)]
-    )
-    error_message = "Tags map should include Owner key string."
-  }
-
-  validation {
-    condition = anytrue(
-      [for obj in keys(var.tags) : contains(["Environment"], obj)]
-    )
-    error_message = "Tags map should include Environment key string."
+    error_message = "Tags map should include data-classification key string."
   }
 }
 
